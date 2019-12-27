deaf5e4663c91dfb2ba60f8435971c2a4b140b5f
pk4pnl <08meszzi@gail.com>
2019-12-27 16:01:10 +0100

    open pics in img dir

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import cv2
 import sys
 import os
+from os import listdir
+from os.path import isfile, join
+import numpy
+
+
 
 def detect(image):
     #read original picture >> img0
@@ -13,6 +18,7 @@ def detect(image):
         exit()
 
     #convert to grayscale >> img1
+    print("3")
     img1 = cv2.cvtColor(img0, cv2.COLOR_BGR2GRAY)
     cv2.namedWindow('grayscale', cv2.WINDOW_AUTOSIZE)
     cv2.imshow('grayscale', img1)
@@ -21,6 +27,7 @@ def detect(image):
         exit()
  
     #add blur to grayscale >> img2
+    print("2")
     img2 = cv2.medianBlur(img1, 15)
     cv2.namedWindow('blur', cv2.WINDOW_AUTOSIZE)
     cv2.imshow('blur', img2)
@@ -29,14 +36,16 @@ def detect(image):
         exit()
 
     #binary threshold to filter noise >> img3
+    print("1")
     ret, img3 = cv2.threshold(img2, 100, 255, 0)
     cv2.namedWindow('bin_thresh', cv2.WINDOW_AUTOSIZE)
     cv2.imshow('bin_thresh', img3)
 
-root = "img"
-for path, subdirs, files in os.walk(root):
-    for name in files:
-        detect(name)
+mypath='img/'
+onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+images = numpy.empty(len(onlyfiles), dtype=object)
+for n in range(0, len(onlyfiles)):
+  images[n] = detect( join(mypath,onlyfiles[n]) )
 
 cv2.waitKey()
 exit()
