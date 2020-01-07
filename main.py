5ec0753a9d591c956d60595cb0660c90a0dcd076
pk4pnl <08meszzi@gail.com>
2020-01-07 19:51:27 +0100

    clean commented code

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ from os import listdir
 from os.path import isfile, join
 import numpy as np
 
-
-
 def detect(image):
-    #read original picture >> img0
+    # read original picture >> img0
     print("filename: ", image)
     img0 = cv2.imread('' + image)
     """
@@ -18,8 +16,7 @@ def detect(image):
     if k == 27:
         exit()
     """
-    #convert to grayscale >> img1
-    #print("3")
+    # convert to grayscale >> img1
     img1 = cv2.cvtColor(img0, cv2.COLOR_BGR2GRAY)
     """
     cv2.namedWindow('grayscale', cv2.WINDOW_AUTOSIZE)
@@ -28,8 +25,7 @@ def detect(image):
     if k == 27:
         exit()
     """
-    #add blur to grayscale >> img2
-    #print("2")
+    # add blur to grayscale >> img2
     img2 = cv2.medianBlur(img1, 19)
     """
     cv2.namedWindow('blur', cv2.WINDOW_AUTOSIZE)
@@ -38,24 +34,22 @@ def detect(image):
     if k == 27:
         exit()
     """
-    #binary threshold to filter noise >> img3
-    #print("1")
+    # binary threshold to filter noise >> img3
     ret, img3 = cv2.threshold(img2, 100, 255, 0)
+    # optional: Gaussian adaptive threshold algorythm
     #img3 = cv2.adaptiveThreshold(img2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 63, 5) 
-    cv2.namedWindow('bin_thresh', cv2.WINDOW_AUTOSIZE)
-    cv2.imshow('bin_thresh', img3)
+    # cv2.namedWindow('bin_thresh', cv2.WINDOW_AUTOSIZE)
+    # cv2.imshow('bin_thresh', img3)
 
     # Read image
-    #im = cv2.imread("blob.jpg", cv2.IMREAD_GRAYSCALE)
     im = img3
     
-    # Setup SimpleBlobDetector parameters.
+    # Setup SimpleBlobDetector parameters. source: https://www.learnopencv.com/blob-detection-using-opencv-python-c/
     params = cv2.SimpleBlobDetector_Params()
 
     # Change thresholds
     params.minThreshold = 10
     params.maxThreshold = 200
-
 
     # Filter by Area.
     params.filterByArea = True
@@ -73,14 +67,7 @@ def detect(image):
     params.filterByInertia = True
     params.minInertiaRatio = 0.01
 
-    # Create a detector with the parameters
-    # ver = (cv2.__version__).split('.')
-    # if int(ver[0]) < 3 :
-    #     detector = cv2.SimpleBlobDetector(params)
-    # else : 
-    #         detector = cv2.SimpleBlobDetector_create(params)
     detector = cv2.SimpleBlobDetector_create(params)
-
 
     # Detect blobs.
     keypoints = detector.detect(im)
@@ -105,17 +92,14 @@ def detect(image):
             1, #font size
             (0, 0, 255), #font color
             3) #font stroke
-        #cv2.imwrite('Keypoints', im_with_keypoints)
-
+        
         # Show blobs
         cv2.imshow("Keypoints", im_with_keypoints)
 
     else:
         print("No dots found, inverting")
         img4 = cv2.bitwise_not(img3)
-        #cv2.namedWindow('inverted_img', cv2.WINDOW_AUTOSIZE)
-        #cv2.imshow('inverted_img', img4)
-
+        
         # Setup SimpleBlobDetector parameters.
         params = cv2.SimpleBlobDetector_Params()
 
@@ -143,12 +127,7 @@ def detect(image):
         params.minInertiaRatio = 0.01
 
         # Create a detector with the parameters
-        ver = (cv2.__version__).split('.')
-        if int(ver[0]) < 3 :
-            detector = cv2.SimpleBlobDetector(params)
-        else : 
-            detector = cv2.SimpleBlobDetector_create(params)
-
+        detector = cv2.SimpleBlobDetector_create(params)
 
         # Detect blobs.
         keypoints = detector.detect(img4)
@@ -171,8 +150,7 @@ def detect(image):
             1, #font size
             (0, 0, 255), #font color
             3) #font stroke
-        #cv2.imwrite('Keypoints', im_with_keypoints)
-
+        
         # Show blobs
         cv2.imshow("Keypoints", im_with_keypoints)
 
@@ -187,7 +165,6 @@ onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 images = np.empty(len(onlyfiles), dtype=object)
 for n in range(0, len(onlyfiles)):
   images[n] = detect( join(mypath,onlyfiles[n]) )
-
 
 cv2.waitKey()
 exit()
